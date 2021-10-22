d2d0d1595dcdd1b99d244bd2b2872150f9b438bf
Luca Di Fazio <highliuk@gmail.com>
2021-10-22 22:45:39 +0200

    Add signup resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const APP_SECRET = "myStrongPassword1234"
+
 function post(parent, args, { prisma }) {
   const { url, description } = args
   return prisma.link.create({
@@ -8,6 +12,24 @@ function post(parent, args, { prisma }) {
   })
 }
 
+async function signup(parent, args, { prisma }) {
+  const { email, password, name } = args
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  })
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return { token, user }
+}
+
 module.exports = {
   post,
+  signup,
 }
